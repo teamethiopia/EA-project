f260f76ff636f5bc09ee2ff4fe34d83132a2c2f0
Zelalem Zergaw <zola145103@gmail.com>
2019-12-01 15:08:32 -0600

    initial commit of EA project

---
+++ Social Network Project/src/main/java/com/mum/edu/cs544/socialnetworkproject/SocialnetworkprojectApplication.java
@@ -0,0 +1,13 @@
+package com.mum.edu.cs544.socialnetworkproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialnetworkprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialnetworkprojectApplication.class, args);
+    }
+
+}
---
+++ Social Network Project/src/test/java/com/mum/edu/cs544/socialnetworkproject/SocialnetworkprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mum.edu.cs544.socialnetworkproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialnetworkprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
