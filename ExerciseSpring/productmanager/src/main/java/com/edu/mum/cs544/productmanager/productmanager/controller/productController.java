56b8943903187fe16e12637f8f0ad65e9e7167a9
Zelalem Zergaw <zola145103@gmail.com>
2019-12-02 18:36:35 -0600

    first phase done last

--- ExerciseSpring/productmanager/src/main/java/com/edu/mum/cs544/productmanager/productmanager/controller/productController.java
+++ ExerciseSpring/productmanager/src/main/java/com/edu/mum/cs544/productmanager/productmanager/controller/productController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -28,6 +30,20 @@ public class productController {
         model.addAttribute("products",product);
         return "newProduct";
     }
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView editForm(@PathVariable(name = "id") long id){
+        ModelAndView modelAndView=new ModelAndView("editForm");
+        Product product=productService.getProduct(id);
+        modelAndView.addObject("product",product);
+        return modelAndView;
+    }
+    @RequestMapping("/delete/{id}")
+    public String deleteForm(@PathVariable(name = "id") long id){
+        productService.deleteById(id);
+        return "redirect:/";
+    }
+
     @RequestMapping(value = "/save",method = RequestMethod.POST)
     public String saveProduct(@ModelAttribute("product")Product product){
     productService.save(product);
